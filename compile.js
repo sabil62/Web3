71a9f681830a586737336d1546e2209f27f6499e
sabil62 <shresthasabil62@gmail.com>
2022-02-06 12:34:16 +0545

    solc compile

--- compile.js
+++ compile.js
@@ -3,7 +3,10 @@ const fs = require("fs");
 const solc = require("solc");
 
 //to specify a path that is universal (works in all OS)
-const inboxPath = path(__dirname, "contract", "Inbox.sol");
+const inboxPath = path.resolve(__dirname, "contract", "Inbox.sol"); //__dirname is root directoryno
 const source = fs.readFileSync(inboxPath, "utf-8");
 
-console.log(solc.compile(source, 1));
+// console.log(solc.compile(source, 1));
+
+//to export  this in other files
+module.exports = solc.compile(source, 1).contracts[":Inbox"];
--- contract/Inbox.sol
+++ contract/Inbox.sol
@@ -1,14 +1,63 @@
-// // pragma solidity ^0.4.17;
+pragma solidity >=0.4.16 <0.9.0;
+//installed (npm install solc@0.4.25)
 
 // contract Inbox{
 //     string public message;
 
-//     function Inbox(string initialMessage) public {
+//     function Inbox(string initialMessage) {
 //         message = initialMessage;
 //     }
 
-//     function setMessage(string newMessage) public {
+//     function setMessage(string newMessage){
 //         message = newMessage;
 //     }
 // }
 
+
+contract Inbox{
+    string public message;
+
+    // constructor(string memory initialMessage) public{
+    //     message = initialMessage;
+    // }
+
+    function setMessage(string memory newMessage) public{
+        message = newMessage;
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+}
+
+
+// contract MyContract {
+//     string value;
+
+//     function get() public view returns (string memory) {
+//         return value;
+//     }
+
+//     function set(string memory _value) public {
+//         value = _value;
+//     }
+
+//     constructor() public {
+//         value = "myValue";
+//     }
+// }
+
+// SPDX-License-Identifier: GPL-3.0
+// pragma solidity >=0.4.16 <0.9.0;
+
+// contract SimpleStorage {
+//     uint storedData;
+
+//     function set(uint x) public {
+//         storedData = x;
+//     }
+
+//     function get() public view returns (uint) {
+//         return storedData;
+//     }
+// }
