e3a780ef183df476ec2a2bedf903f4281ca2a7b4
sabil62 <shresthasabil62@gmail.com>
2022-02-06 17:39:45 +0545

    ganauche accounts describe beforeeach

--- test/Input.test.js
+++ test/Input.test.js
@@ -2,30 +2,41 @@ const assert = require("assert");
 const ganache = require("ganache");
 const Web3 = require("web3");
 
-// const web3 = Web3(ganache.provider());
+const web3 = new Web3(ganache.provider());
+
+beforeEach(() => {
+  //get a list of all acounts
+  web3.eth.getAccounts().then((fetchedAccounts) => {
+    console.log(fetchedAccounts);
+  });
+});
+
+describe("web3 ganauche", () => {
+  it("deploy contract", () => {});
+});
 
 //just testing
-class Car {
-  cars() {
-    return "cars";
-  }
-  bus() {
-    return "bus";
-  }
-}
+// class Car {
+//   cars() {
+//     return "cars";
+//   }
+//   bus() {
+//     return "bus";
+//   }
+// }
 
-let car;
-beforeEach(() => {
-  car = new Car();
-});
+// let car;
+// beforeEach(() => {
+//   car = new Car();
+// });
 
-describe("mocha test", () => {
-  it("car", () => {
-    assert.equal(car.cars(), "cars");
-  });
-  it("bus", () => {
-    assert.equal(car.bus(), "bus");
-  });
-});
+// describe("mocha test", () => {
+//   it("car", () => {
+//     assert.equal(car.cars(), "cars");
+//   });
+//   it("bus", () => {
+//     assert.equal(car.bus(), "bus");
+//   });
+// });
 
 // "test": "echo \"Error: no test specified\" && exit 1"
