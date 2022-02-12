0e6fcf874fc70ce9fe67ee99959fc4fea22e9b1e
sabil62 <shresthasabil62@gmail.com>
2022-02-12 16:33:04 +0545

    deployed contract to rinkeby network rufflehdwallet mnemonic provider web3 contract gas json

--- deploy.js
+++ deploy.js
@@ -12,3 +12,24 @@ const provider = new HDWalletProvider({
   providerOrUrl:
     "https://rinkeby.infura.io/v3/2aa898d3dbb04587a4542c8f7cace42d",
 });
+
+const web3 = new Web3(provider);
+
+//ABI(interface) and bytecode
+const { interface, bytecode } = require("./compile");
+
+let accounts;
+//connect to network or main code starts here
+const deploy = async () => {
+  accounts = await web3.eth.getAccounts();
+  //   console.log(accounts);
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["random"] })
+    .send({ from: accounts[0], gas: "1000000" });
+
+  console.log("Contract Deployed to ", result.options.address);
+};
+
+deploy();
